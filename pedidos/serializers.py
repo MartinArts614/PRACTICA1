1f88399b24af2bcf7a8e199a42bb9a0d24e1e6fe
Martin614Arts <jrojasg0111@outlook.com>
2026-08-13 21:57:28 -0600

    hacer pull

---
+++ pedidos/serializers.py
@@ -0,0 +1,13 @@
+#Serializer conversion de datos a JSON
+from rest_framework import serializers
+from pedidos.models import Producto, Pedido
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = '__all__'
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
