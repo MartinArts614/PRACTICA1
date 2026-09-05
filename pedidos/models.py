62f6b047f8534909beaf53ef867bedd54bc63ff9
Martin614Arts <jrojasg0111@outlook.com>
2026-09-04 19:07:57 -0600

    actualización 040926

--- pedidos/models.py
+++ pedidos/models.py
@@ -14,12 +14,16 @@ class Producto(models.Model):
         ('POSTRE', 'Postre'),
     ]
     nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    #Validación de precio positivo
+    precio = models.DecimalField(max_digits=6, decimal_places=2,
+                                 validators=[validar_precio_positivo])
     categoria = models.CharField(max_length=10, choices=CATEGORIAS)
     disponible = models.BooleanField(default=True)
 
 # Soporte para archivos multimedia (Media Files)
     imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+
+    #agregar pdf|
     
     def __str__(self):
         return f"{self.nombre} - ${self.precio}"
@@ -32,6 +36,14 @@ class Pedido(models.Model):
         ('ENTREGADO', 'Entregado'),
     ]
     cliente_nombre = models.CharField(max_length=100)
+    #Relacion nombre con producto
+    producto = models.ForeignKey(
+        Producto, 
+        on_delete=models.CASCADE, 
+        related_name='pedidos',
+        null=True,
+        blank=True
+    )
     fecha = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=15, choices=ESTADOS, default='PENDIENTE')
     total = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
